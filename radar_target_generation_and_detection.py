2b30e56bc6314173e0243470b0f82627e087d719
Vladimir-Lazic <vladimirzlazic@gmail.com>
2021-04-25 15:26:51 +0200

    Calculate slope

--- radar_target_generation_and_detection.py
+++ radar_target_generation_and_detection.py
@@ -13,13 +13,27 @@ speed of light = 3e8
 User Defined Range and Velocity of target
 """
 
+speed_of_light = 3e8
+target_distance = 110
+target_velocity = -20
+
 class Radar:
-      def __init__(self, frequency, max_range, range_resolution, max_velocity):
-          self.frequency = frequency
-          self.max_range = max_range
-          self.range_resolution = range_resolution
-          self.max_velocity = max_velocity
+    def __init__(self, frequency, max_range, range_resolution, max_velocity):
+        self.frequency = frequency
+        self.max_range = max_range
+        self.range_resolution = range_resolution
+        self.max_velocity = max_velocity
+
+    def get_bandwidth(self):
+        return speed_of_light / (2 * self.range_resolution)
+
+    def get_chirp(self):
+        return (5.5 * 2 * self.max_range) / speed_of_light 
+
+    def get_slope(self):
+        return self.get_bandwidth() / self.get_chirp()
 
 
 radar = Radar(77, 200, 1, 100)
 
+print("Get slope: ", radar.get_slope())
